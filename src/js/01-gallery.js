55b5541584c889d6f687794e839f5282fe31b89d
kbugayenko <k.bugayenko@gmail.com>
2022-11-17 23:55:52 +0200

    add 01+02

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,33 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `
+    <a class="gallery__item" style="display: block" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+`
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
+
+function onClickGallery(e) {
+  e.preventDefault();
+  gallery.on('show.simplelightbox', function () {});
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,48 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+if (!localStorage.getItem(LOCALSTORAGE_KEY)) {
+  localStorage.setItem(LOCALSTORAGE_KEY, 0);
+}
+
+player
+  .setCurrentTime(load(LOCALSTORAGE_KEY))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
+
+player.on('timeupdate', throttle(timeOnNow, 1000));
+
+function timeOnNow(e) {
+  localStorage.setItem(LOCALSTORAGE_KEY, e.seconds);
+}
