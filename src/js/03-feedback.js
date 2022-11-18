b71db7e29a5d5d162e61933feff77c5e726a72f0
kbugayenko <k.bugayenko@gmail.com>
2022-11-19 01:15:33 +0200

    last update 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,53 +1,34 @@
 import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
-const form = document.querySelector('.feedback-form');
-const input = document.querySelector('.js-input');
-const message = document.querySelector('.js-message');
+updateFormField();
 
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
-let storageValue = {};
-
-const load = key => {
-  try {
-    const serializedState = localStorage.getItem(key);
-    return serializedState === null ? undefined : JSON.parse(serializedState);
-  } catch (error) {
-    console.error('Get state error: ', error.message);
-  }
-};
-
-if (!localStorage.getItem(LOCALSTORAGE_KEY)) {
-  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(storageValue));
-}
-
-if (load(LOCALSTORAGE_KEY).email) {
-  input.value = load(LOCALSTORAGE_KEY).email;
-}
-if (load(LOCALSTORAGE_KEY).message) {
-  message.value = load(LOCALSTORAGE_KEY).message;
-}
 
 function onFormSubmit(e) {
   e.preventDefault();
-  console.log('Відправлення форми');
-
-  const obj = {
-    email: input.value,
-    message: message.value,
-  };
-  console.log('Obj: ', obj);
-
-  input.value = '';
-  message.value = '';
-  storageValue = {};
+  const data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (!data.message || !data.email) return;
+  console.log(data);
   localStorage.removeItem(LOCALSTORAGE_KEY);
+  updateFormField();
 }
 
-function onFormInput(e) {
-  storageValue[e.target.name] = e.target.value;
+function onFormInput({ target: { name, value } }) {
+  const store = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
+  store[name] = value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(store));
+}
 
-  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(storageValue));
+function updateFormField() {
+  const data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  Object.entries(data || { email: '', message: '' }).forEach(
+    ([name, value]) => {
+      form.elements[name].value = value;
+    }
+  );
 }
